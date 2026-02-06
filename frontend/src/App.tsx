98f4f6cee4ce6a4b1ca063806dbf0a13ceb7a9b1
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2026-02-07 01:46:27 +0300

    feat: config PKCE for auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,12 @@ const keycloak = new Keycloak(keycloakConfig);
 
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider authClient={keycloak} initOptions={{
+      onLoad: 'check-sso',
+      flow: 'standard',
+      pkceMethod: 'S256',
+      silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
+    }}>
       <div className="App">
         <ReportPage />
       </div>

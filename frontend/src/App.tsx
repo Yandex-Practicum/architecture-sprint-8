8e8e25082c2545f0996642512d44a0a253d3231e
Ivan Arkhipov <i.arkhipov@dm-solutions.ru>
2026-04-02 00:19:32 +0300

    Task1.2 PKCE added to front and keycloak apps

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,11 +9,16 @@ const keycloakConfig: KeycloakConfig = {
   clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||""
 };
 
+const initOptions = {
+  onLoad: undefined,  // помогает избавиться от циклической перезагрузки при запуске
+  pkceMethod: 'S256'  // Включение PKCE
+};
+
 const keycloak = new Keycloak(keycloakConfig);
 
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions}>
       <div className="App">
         <ReportPage />
       </div>

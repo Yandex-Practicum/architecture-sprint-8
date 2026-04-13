e70ec0202554a8545b21dd855a28905bd6ad6511
Konstantin podobryanskiy <kostet19940308@gmail.com>
2026-04-13 12:47:15 +0300

    Task1.2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,9 +11,15 @@ const keycloakConfig: KeycloakConfig = {
 
 const keycloak = new Keycloak(keycloakConfig);
 
+const initOptions = {
+  onLoad: 'check-sso',
+  flow: 'standard',
+  pkceMethod: 'S256'
+};
+
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions}>
       <div className="App">
         <ReportPage />
       </div>
